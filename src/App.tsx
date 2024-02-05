8dac0c19a64b8cbd9d80ce9ca034be3fbd5f6e38
mackenneth <mackenneth@yandex.ru>
2024-02-05 23:55:37 +0300

    feat: add dark and light themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import {Routes} from "react-router-dom";
-
+import "./styles/index.scss";
 export const App = () => {
     return (
-        <div>
-            <Routes>
-            </Routes>
+        <div className="app">
+            Hello world
         </div>
     )
 }
