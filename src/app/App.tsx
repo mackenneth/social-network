eb7268202b1b7e4a35ba2abb059c9db9fad7c629
ne-maksim <mackenneth@yandex.ru>
2024-02-07 00:22:15 +0300

    refactor: change structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
 import "./styles/index.scss";
-import {useTheme} from "./theme/useTheme";
-
+import {useTheme} from "@/app/providers/theme-provider";
+import {classNames} from "@/shared/lib/classNames";
 
 export const App = () => {
 
     const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {},[theme])}>
             Hello world
             <button onClick={toggleTheme}>
                 Toggle {theme}
