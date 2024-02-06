96ebc1fe13c35792201885e87ec2210c6df756e7
ne-maksim <mackenneth@yandex.ru>
2024-02-06 22:51:52 +0300

    feat: add className helper

---
+++ src/helpers/classNames.ts
@@ -0,0 +1,10 @@
+export const classNames = (className: string, mods: Record<string, boolean | string> = {}, additional: string[] = []): string => {
+    return [
+        className,
+        ...Object.entries(mods)
+            .filter(([cls, value]) => !!value)
+            .map(([cls]) => cls),
+        ...additional
+    ].join(' ')
+
+}
