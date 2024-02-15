76770c9f5101e7ebb6de29875c7df51982896d4d
ne-maksim <mackenneth@yandex.ru>
2024-02-15 22:49:40 +0300

    feat: use custom button for theme switcher

--- src/shared/ui/theme-switcher/ThemeSwitcher.tsx
+++ src/shared/ui/theme-switcher/ThemeSwitcher.tsx
@@ -3,6 +3,7 @@ import {useTheme} from "@/shared/lib/theme/useTheme";
 import {Theme} from "@/shared/lib/theme/ThemeContext";
 import LightThemeIcon from "@/shared/assets/icons/theme-light.svg";
 import DarkThemeIcon from "@/shared/assets/icons/theme-night.svg";
+import {Button, ButtonVariant} from "@/shared/ui/Button/Button";
 interface ThemeSwitcherProps {
     className?: string
 }
@@ -12,8 +13,11 @@ export const ThemeSwitcher = (props: ThemeSwitcherProps) => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <button onClick={toggleTheme} className={classNames('ThemeSwitcher', {}, [className])}>
+        <Button onClick={toggleTheme}
+                className={classNames('', {}, [className])}
+                variant={ButtonVariant.CLEAR}
+        >
             {theme === Theme.DARK ? <LightThemeIcon fill="#D0D0D0"/> : <DarkThemeIcon fill="000"/>}
-        </button>
+        </Button>
     )
 }
