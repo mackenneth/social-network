fdc69f135d5721e40ae673f7cd9d84b743c72b55
ne-maksim <mackenneth@yandex.ru>
2024-02-21 23:19:42 +0300

    feat: config i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,18 +4,20 @@ import {AppRouter} from "@/app/providers/router";
 import {NavBar} from "@/widgets/nav-bar";
 import {useTheme} from "@/shared/lib/theme/useTheme";
 import {Sidebar} from "@/widgets/sidebar";
-
+import {Suspense} from "react";
 
 export const App = () => {
     const {theme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <NavBar/>
-            <div className={'content-page'}>
-                <Sidebar />
-                <AppRouter className={'content'}/>
-            </div>
+            <Suspense fallback={'loading...'}>
+                <NavBar/>
+                <div className={'content-page'}>
+                    <Sidebar />
+                    <AppRouter className={'content'}/>
+                </div>
+            </Suspense>
         </div>
     )
 }
